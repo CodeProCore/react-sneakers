bfbb812b6fcafb2051919628b44b1536d4949dda
CodeProCore <for4sudyfor4job@gmail.com>
2022-06-15 21:28:22 +0300

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+
+
+function App() {
+  return ( 
+    <div className="wrapper"> 
+              <header>
+              <div className="headerLeft">
+                  <svg />
+                  <div className="headerInfo"> 
+                    <h3> React Sneakers</h3>
+                    <p> Магазин лучших кроссовок</p>
+                  </div>
+                </div>
+                <ul className="headerRight">
+                  <li>
+                    <svg />
+                    <span> 1205 грн.</span>
+                  </li>
+                  <li>
+                    <svg />        
+                  </li>
+                </ul>  
+              </header>
+              <div className="content">
+                <h1>Все кроссовки</h1>
+              </div>
+  
+            
+  </div>
+ );  
+  
+}
+
+export default App;
